93f526d6d99b100359f8fdeb6d161cacb1f41006
mathurasridhar <mathura1987@gmail.com>
2025-04-28 23:30:01 -0400

    Bitbucket implementation

--- app/src/Service/fetchBBDao.py
+++ app/src/Service/fetchBBDao.py
@@ -1,17 +1,18 @@
 from dotenv import load_dotenv
 import os
 import json 
-from flask import jsonify
+from flask import jsonify, abort
 import logging
 import requests
 
 
 def getBBData(reponame):
     url_bitbucket = f'https://api.bitbucket.org/2.0/repositories/{reponame}'
-    try:
-        response = requests.get(url_bitbucket)
+
+    response = requests.get(url_bitbucket)
+    if response.status_code == 200:
         data = response.json()
-        bb_public_repos = data.json()
+        bb_public_repos = data
         bb_public_repos = bb_public_repos['values']
         bb_repos = []
         bb_forked = []
@@ -35,6 +36,7 @@ def getBBData(reponame):
         rres = {}
         rres['data'] = [{'bb_public_repos_orig':bb_repos, 'bb_forked_repos':bb_forked, 'bb_watchers':bb_watcher_cnt, 'bb_languages':len(list(set(bb_languages)))}]
         return rres
-    except requests.exceptions.HTTPError as errh:
-        print("Http Error:", errh.args[0])
+    else:
+        abort(response.status_code, response.text)
+   
 
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app.routes import app
-from app.src.controllers import gitController
+from app.src.controllers import gitController, bbController
 from flask import Flask , jsonify
 import json
 
@@ -10,9 +10,16 @@ def home():
     return '<h1>Flask Rest API</h1>'
 
 @app.route('/api/git=<reponame>', methods=['GET'])
-def getProfileData(reponame):
+def getProfileDataGit(reponame):
     response = gitController.fetchGitDao.getGitData(reponame)
     return jsonify(response)
+
+@app.route('/api/bb=<reponame>', methods=['GET'])
+def getProfileDataBitbucket(reponame):
+    response = bbController.fetchBBDao.getBBData(reponame)
+    return jsonify(response)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
